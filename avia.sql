147b12f4e08da5a23ff995d2191c9f72ad738484
Elesemushina <71425862+Elesemushina@users.noreply.github.com>
2021-01-31 19:49:14 +0300

    Update avia.sql

--- avia.sql
+++ avia.sql
@@ -22,7 +22,25 @@ join dst_project.aircrafts s on s.aircraft_code=f.aircraft_code
 join dst_project.seats se on s.aircraft_code=se.aircraft_code
 where a_from.city = 'Anapa' and actual_departure>= '2016-12-01' and actual_departure< '2017-03-01'
 group by 1,2
------в Москву летает 11700 человек, Белгород - 8730, Новокузнецк - 1690. 
+-----Максимальная загрузка самолетов: Москва 11700 человек, Белгород - 8730, Новокузнецк - 1690.
+
+----фактическая загрузка самолетов
+select  a_to.city, f.arrival_airport, count(tf.flight_id)
+from dst_project.flights f
+join dst_project.aircrafts s on s.aircraft_code=f.aircraft_code
+join dst_project.airports a_from on a_from.airport_code= f.departure_airport
+join dst_project.airports a_to on a_to.airport_code= f.arrival_airport
+join dst_project.ticket_flights tf on tf.flight_id = f.flight_id
+where f.departure_airport = 'AAQ' and f.actual_departure>= '2016-12-01' and f.actual_departure< '2017-03-01'
+group by 1,2
+---
+/*
+итоги:
+Belgorod | EGO | 8141
+Moscow   |SVO  |10210
+
+Ни один человек не летаел зимой в Новокузнецк
+*/
 
 ----наиболее частые перелеты
 select date_trunc('month',actual_departure), extract(hour from scheduled_departure),  a_to.city,count(flight_id)
@@ -47,12 +65,12 @@ group by 1,2,3
 
 -----прибыльность рейсов: кол-во билетов/ размер топлива
 /* средний расход топлива в зависимости от модели:
-Bombardier CRJ-200: 1 200 кг/час
+Bombardier CRJ-200: 1 200 кг/час : 1200/60 = 20 кг/мин
 Boeing 767-300: 4 500 кг/час
 Cessna 208 Caravan: 1257 кг/час
-Boeing 737-300:  2400 кг/час
+Boeing 737-300:  2400 кг/час : 2400/60 = 40 кг/мин
 Airbus A321-200: 3 200 кг/час
-Sukhoi Superjet-100: 1700 кг/час
+Sukhoi Superjet-100: 1700 кг/час : 1700/60 = 28,3 кг/мин
 
 Цена топлива в Анапе в 2017 году в январе:41 435 руб за тонну; в феврале: 39 553 руб за тонну; в декабре 2016: 38 867 руб за тонну, 
 среднее значение:  39,952 за кг
@@ -72,3 +90,57 @@ Belgorod 0 years 0 mons 0 days 0 hours 50 mins 0.00 secs
 Moscow 0 years 0 mons 0 days 1 hours 40 mins 0.00 secs
 Novokuznetsk 0 years 0 mons 0 days 5 hours 5 mins 0.00 secs
 */
+----все перелеты в Мск и Белгород прибыльные. При чем прибыль в 8-9 раз больше расходов
+select city, 
+       model,
+       flight_id, 
+       profit - costs as pnl,  
+       (profit - costs)/costs as margin
+from (select  a_to.city,
+        s.model, 
+        f.flight_id,
+        case when model = 'Sukhoi Superjet-100'and a_to.city = 'Belgorod' 
+                  then 39.952*28.3*50
+                  when  model = 'Boeing 737-300' and a_to.city = 'Moscow' 
+                  then 39.952*40*100
+                  end as costs,
+         sum(amount)  as profit
+from dst_project.flights f
+join dst_project.aircrafts s on s.aircraft_code=f.aircraft_code
+join dst_project.airports a_from on a_from.airport_code= f.departure_airport
+join dst_project.airports a_to on a_to.airport_code= f.arrival_airport
+join dst_project.ticket_flights tf on tf.flight_id = f.flight_id
+where f.departure_airport = 'AAQ' and f.actual_departure>= '2016-12-01' and f.actual_departure< '2017-03-01'
+group by 1,2,3,4
+) a
+order by 4  
+
+
+-----убыточны 4 рейса в Новокузнецк никто не летает, т.е расходы составляют: 487,414.4 руб за один перелет
+select model, flight_id, 39.952*40*305 as cost ---39 цена на 1 кг топлива, 305 минут лететь, 40 кг за минуту перелета
+from dst_project.flights f
+join dst_project.airports a_from on a_from.airport_code= f.departure_airport
+join dst_project.airports a_to on a_to.airport_code= f.arrival_airport
+join dst_project.aircrafts s on s.aircraft_code=f.aircraft_code
+where a_from.city = 'Anapa' 
+      and a_to.city = 'Novokuznetsk'
+      and actual_departure >= '2016-12-01'
+      and actual_departure< '2017-03-01' 
+      
+      
+ /*т.е нужно убрать вылеты в Новокузнецк: рейсы c flight_id:
+136511
+136513
+136514
+136518
+136523
+136533
+136534
+136540
+136544
+136546
+136560
+136564
+136567
+    */
+
