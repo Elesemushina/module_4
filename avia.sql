666930ffc1f4535c3a36e4478da92250c09a139d
Elesemushina <71425862+Elesemushina@users.noreply.github.com>
2021-01-31 20:37:28 +0300

    Update avia.sql

--- avia.sql
+++ avia.sql
@@ -1,37 +1,47 @@
 -----наиболее популярное направление: по кол-ву запланированных рейсов зимой
-select a_to.city, model, count(distinct flight_id)
+select a_to.city, 
+       model, 
+       count(distinct flight_id)
 from dst_project.flights f
 join dst_project.airports a_from on a_from.airport_code= f.departure_airport
 join dst_project.airports a_to on a_to.airport_code= f.arrival_airport
 join dst_project.aircrafts s on s.aircraft_code=f.aircraft_code
 join dst_project.seats se on s.aircraft_code=se.aircraft_code
-where a_from.city = 'Anapa' and 
-scheduled_departure >= '2016-12-01'
-and scheduled_departure< '2017-03-01' --- такой же запрос по фактическому отправлению actual_departure>= '2016-12-01' and actual_departure< '2017-03-01'
+where a_from.city = 'Anapa' 
+      and scheduled_departure >= '2016-12-01'
+      and scheduled_departure< '2017-03-01' --- такой же запрос по фактическому отправлению actual_departure>= '2016-12-01' and actual_departure< '2017-03-01'
 group by 1,2
 ---вывод: в Белгород и Москву летает по 90 рейсов, в Новокузнецк только 13, в Новокузнецк летает только Boeing 737-300
 ---из Анапы летает только две модели самолетов: Boeing 737-300, Sukhoi Superjet-100
 
 
 ---по кол-ву чел-к, наименее популярные модели рейсов
-select a_to.city, model, count(*)
+select a_to.city, 
+       model, 
+       count(*)
 from dst_project.flights f
 join dst_project.airports a_from on a_from.airport_code= f.departure_airport
 join dst_project.airports a_to on a_to.airport_code= f.arrival_airport
 join dst_project.aircrafts s on s.aircraft_code=f.aircraft_code
 join dst_project.seats se on s.aircraft_code=se.aircraft_code
-where a_from.city = 'Anapa' and actual_departure>= '2016-12-01' and actual_departure< '2017-03-01'
+where a_from.city = 'Anapa' 
+      and actual_departure>= '2016-12-01' 
+      and actual_departure< '2017-03-01'
 group by 1,2
 -----Максимальная загрузка самолетов: Москва 11700 человек, Белгород - 8730, Новокузнецк - 1690.
 
 ----фактическая загрузка самолетов
-select  a_to.city, f.arrival_airport, count(tf.flight_id)
+select  a_to.city, 
+        f.arrival_airport, 
+        count(tf.flight_id)
 from dst_project.flights f
 join dst_project.aircrafts s on s.aircraft_code=f.aircraft_code
 join dst_project.airports a_from on a_from.airport_code= f.departure_airport
 join dst_project.airports a_to on a_to.airport_code= f.arrival_airport
 join dst_project.ticket_flights tf on tf.flight_id = f.flight_id
-where f.departure_airport = 'AAQ' and f.actual_departure>= '2016-12-01' and f.actual_departure< '2017-03-01'
+where f.departure_airport = 'AAQ' ---аэропорт Анапы
+      and f.actual_departure>= '2016-12-01'
+      and f.actual_departure< '2017-03-01'
 group by 1,2
 ---
 /*
@@ -53,24 +63,33 @@ and scheduled_departure >= '2016-12-01'
 and scheduled_departure< '2017-03-01'
 
 ----наиболее частые перелеты
-select date_trunc('month',actual_departure), extract(hour from scheduled_departure),  a_to.city,count(flight_id)
+select date_trunc('month',actual_departure), 
+       extract(hour from scheduled_departure), 
+       a_to.city,
+       count(flight_id)
 from dst_project.flights f
 join dst_project.airports a_from on a_from.airport_code= f.departure_airport
 join dst_project.airports a_to on a_to.airport_code= f.arrival_airport
 join dst_project.aircrafts s on s.aircraft_code=f.aircraft_code
-where a_from.city = 'Anapa' and actual_departure>= '2016-12-01' and actual_departure< '2017-03-01'
+where a_from.city = 'Anapa' 
+      and actual_departure>= '2016-12-01' 
+      and actual_departure< '2017-03-01'
 group by 1,2,3
 ----вывод: в мск и белгород самолеты летают каждый день, в Новокузнецк только раз в неделю в 6 утра. В Мск и Белгород в 9-10 утра
 
 ----в Новокузнецк летают только по вторникам в 6 утра (хотела использовать dateweek функцию, но она не работает в metabase)
-select scheduled_departure, extract(hour from scheduled_departure), a_to.city,count(flight_id)
+select scheduled_departure, 
+        extract(hour from scheduled_departure), 
+        a_to.city,
+        count(flight_id)
 from dst_project.flights f
 join dst_project.airports a_from on a_from.airport_code= f.departure_airport
 join dst_project.airports a_to on a_to.airport_code= f.arrival_airport
 join dst_project.aircrafts s on s.aircraft_code=f.aircraft_code
-where a_from.city = 'Anapa' and actual_departure>= '2016-12-01' 
-and actual_departure< '2017-03-01'
-and a_to.city not in ('Moscow', 'Belgorod')
+where a_from.city = 'Anapa' 
+      and actual_departure>= '2016-12-01' 
+      and actual_departure< '2017-03-01'
+      and a_to.city not in ('Moscow', 'Belgorod')
 group by 1,2,3
 
 -----прибыльность рейсов: кол-во билетов/ размер топлива
@@ -88,12 +107,15 @@ Sukhoi Superjet-100: 1700 кг/час : 1700/60 = 28,3 кг/мин
 
 
 -----время перелета (хотела использовать datediff или timediff но ее нет в metabase)
-select  distinct a_to.city, scheduled_arrival- scheduled_departure
+select  distinct a_to.city, 
+        scheduled_arrival-scheduled_departure
 from dst_project.flights f
 join dst_project.airports a_from on a_from.airport_code= f.departure_airport
 join dst_project.airports a_to on a_to.airport_code= f.arrival_airport
 join dst_project.aircrafts s on s.aircraft_code=f.aircraft_code
-where a_from.city = 'Anapa' and actual_departure>= '2016-12-01' and actual_departure< '2017-03-01'
+where a_from.city = 'Anapa' 
+      and actual_departure>= '2016-12-01' 
+      and actual_departure< '2017-03-01'
 
 /*
 Belgorod 0 years 0 mons 0 days 0 hours 50 mins 0.00 secs
@@ -120,7 +142,9 @@ join dst_project.aircrafts s on s.aircraft_code=f.aircraft_code
 join dst_project.airports a_from on a_from.airport_code= f.departure_airport
 join dst_project.airports a_to on a_to.airport_code= f.arrival_airport
 join dst_project.ticket_flights tf on tf.flight_id = f.flight_id
-where f.departure_airport = 'AAQ' and f.actual_departure>= '2016-12-01' and f.actual_departure< '2017-03-01'
+where f.departure_airport = 'AAQ' 
+       and f.actual_departure>= '2016-12-01' 
+       and f.actual_departure< '2017-03-01'
 group by 1,2,3,4
 ) a
 order by 4  
